3d675d71e239bef9da6f60dc6e078520e25afc98
koikekatsumi <katsumikoike724@gmail.com>
2024-08-08 06:46:24 +0900

    doNothigの記述追加による修正

--- src/test/java/com/example/movie_list/MovieListServiceTest.java
+++ src/test/java/com/example/movie_list/MovieListServiceTest.java
@@ -16,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -39,7 +40,6 @@ public class MovieListServiceTest {
     @Test
     public void 存在する映画リストのIDを指定したとき正常に映画リストが返されること() throws Exception {
         doReturn(Optional.of(new Movie(1, "ホーム　アローン", LocalDate.of(1991, 06, 22), "マコーレ　カリキン", 476684675))).when(movieListMapper).findById(1);
-
         Movie actual = movieListService.findMovie(1);
         assertThat(actual).isEqualTo(new Movie(1, "ホーム　アローン", LocalDate.of(1991, 06, 22), "マコーレ　カリキン", 476684675));
     }
@@ -68,16 +68,19 @@ public class MovieListServiceTest {
 
     @Test
     public void 新しい映画リストを登録すること() {
-        Movie movie = new Movie("パイレーツ　オブ　カリビアン", LocalDate.of(2003, 07, 18), "ジョニー　デップ", 654264015);
-        assertThat(movieListService.insert("パイレーツ　オブ　カリビアン", LocalDate.of(2003, 07, 18), "ジョニー　デップ", 654264015)).isEqualTo(movie);
-        verify(movieListMapper).insert(movie);
+        Movie newMovie = new Movie("パイレーツ　オブ　カリビアン", LocalDate.of(2003, 7, 18), "ジョニー　デップ", 654264015);
+        doNothing().when(movieListMapper).insert(newMovie);
+        assertThat(movieListService.insert("パイレーツ　オブ　カリビアン", LocalDate.of(2003, 7, 18), "ジョニー　デップ", 654264015))
+                .isEqualTo(newMovie);
+        verify(movieListMapper).insert(newMovie);
     }
 
     @Test
     public void 存在する映画リストを削除すること() {
         int validId = 4;
         Movie existingMovie = new Movie(4, "バック　トゥ　ザ　フューチャー", LocalDate.of(1985, 12, 7), "マイケル　J　フォックス", 210609762);
-        when(movieListMapper.findById(4)).thenReturn(Optional.of(existingMovie));
+        when(movieListMapper.findById(validId)).thenReturn(Optional.of(existingMovie));
+        doNothing().when(movieListMapper).delete(validId);
         movieListService.delete(validId);
         verify(movieListMapper).delete(validId);
     }
